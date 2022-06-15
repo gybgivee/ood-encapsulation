64cc7a464cda04c6f466a225356b5c9ea4c48b41
gybgivee <kmcn0512@gmail.com>
2022-06-15 13:14:14 +0100

    no-node

---
+++ src/secretDiary.js
@@ -0,0 +1,57 @@
+class SecretDiary {
+
+    constructor(diary = [], lock = true) {
+        this.diary = diary;
+        this.lock = lock;
+    }
+    getDiary() {
+        return this.diary;
+    }
+    setDiary(diary) {
+        this.diary = diary;
+    }
+    getLock() {
+        return this.lock;
+    }
+    setLock(lock) {
+        this.lock = lock;
+    }
+    addEntry(entry) {
+        const diaryStatus = this.getLock();
+        let updateDiary = this.getDiary();
+        if (!diaryStatus) {
+            updateDiary.push(
+                {
+                    text:entry
+
+                });
+            this.setDiary(updateDiary)
+            return this.getDiary();
+        } else {
+            console.log('Entries is locked');
+            return this.getDiary();
+        }
+    }
+    lockDiary(){
+        this.setLock(true);
+    }
+    unlock(){
+        this.setLock(false);
+    }
+    
+    getEntries(){
+        console.log('List of entries in secretDiary :');
+        return this.diary;
+    }
+}
+module.exports = SecretDiary;
+
+const mySecretDiary = new SecretDiary;
+console.log(mySecretDiary.addEntry('go to Bingo'));
+mySecretDiary.unlock();
+console.log(mySecretDiary.addEntry('Coding'));
+console.log('hello');
+mySecretDiary.lockDiary();
+console.log(mySecretDiary.addEntry('Do the Laudry'));
+
+
